37ce90d1b65534971203464ec88e801b817b00f8
Ever-VC <ever.vasquezc27@gmail.com>
2026-02-03 14:41:49 -0600

    🎉 Initial Commit: Base project

---
+++ src/test/java/com/evervc/springboot/app/aop/AopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evervc.springboot.app.aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

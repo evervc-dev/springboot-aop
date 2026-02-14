50847674566564d899f8638f3cfe30bbe36e9c84
Ever-VC <ever.vasquezc27@gmail.com>
2026-02-14 12:04:47 -0600

    ✨ feat: Add GreetingFooAspect and configure aspect ordering
    - Create `GreetingFooAspect` with `@Order(1)` containing `@Before` and `@After` advice
    - Add `@Order(2)` annotation to `GreetingAspect` to establish execution sequence relative to the new aspect

---
+++ src/main/java/com/evervc/springboot/app/aop/aop/GreetingFooAspect.java
@@ -0,0 +1,40 @@
+package com.evervc.springboot.app.aop.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Order(1)
+@Component
+@Aspect
+public class GreetingFooAspect {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Pointcut("execution(String com.evervc.springboot.app.aop.services.IGreetingService.sayHello(..))")
+    public void greetingMethods(){}
+
+    @Before("greetingMethods()")
+    public void loggerBefore(JoinPoint joinPoint) {
+        String method = joinPoint.getSignature().getName();
+        String args = Arrays.toString(joinPoint.getArgs());
+        logger.info("Método:: [" + method + "] invocado con los parametros:: {" + args + "}");
+    }
+
+    // Este método se invoca al final de todos, al tener el valor de la notación @Order en 1
+    // es como si envolviera la ejecución de el que tiene el valor 2
+    @After("greetingMethods()")
+    public void loggerAfter(JoinPoint joinPoint) {
+        String method = joinPoint.getSignature().getName();
+        String args = Arrays.toString(joinPoint.getArgs());
+        logger.info("Después del método:: [" + method + "] invocado con los parametros:: {" + args + "}");
+    }
+}
